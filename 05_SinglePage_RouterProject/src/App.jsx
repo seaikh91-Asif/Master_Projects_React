0c0d82257aa02ffc8d79c058bb8911687e9188d6
seaikh91-Asif <seaikh91@gmail.com>
2026-08-11 22:21:37 +0530

    simgel page application react routing concept

---
+++ 05_SinglePage_RouterProject/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+
+      <Navbar/>
+
+      
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element = {<About/>} />
+          <Route path='/contact' element = {<Contact/>} />
+        </Routes>
+    </div>
+  )
+}
+
+export default App
